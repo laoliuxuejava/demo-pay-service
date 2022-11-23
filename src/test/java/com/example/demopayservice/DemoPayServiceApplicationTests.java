841eb038dc3bf92fa1eba96da5c07aa05a563922
hand_cheng.xing <cheng.xing@hand-china.com>
2022-11-23 17:50:11 +0800

    payservice上传

---
+++ src/test/java/com/example/demopayservice/DemoPayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demopayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
